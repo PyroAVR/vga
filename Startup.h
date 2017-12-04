448e5511284474b0f57df36f3f2a3ce20bf4db0f
Piers <pik3387@rit.edu>
2017-12-03 19:14:21 -0500

    Initialized gpio

--- Startup.h
+++ Startup.h
@@ -2,9 +2,11 @@
 #ifndef __STARTUP_H__
 #define __STARTUP_H__
 /* C function declarations */
-
+														
 //parameterize these
 void init_dac0(void);
 void init_sync_signals(void);
 void init_gpio(void);
 #endif
+
+
